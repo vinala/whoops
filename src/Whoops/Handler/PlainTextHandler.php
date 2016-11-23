3e926c25ffa49e573a3f7f2accb6d7062d92a763
Youssef <youssefhad2@gmail.com>
2016-11-23 23:26:45 +0000

    update the default view to show

--- src/Whoops/Handler/PlainTextHandler.php
+++ src/Whoops/Handler/PlainTextHandler.php
@@ -351,38 +351,48 @@ class PlainTextHandler extends Handler
                     $bg_color=$this->bg_color;
                     //include_once '/core/Access/ErrorDisplayer/simple.php';
                     ?>
-                    <head>
-                        <meta charset="utf-8"/>
-                        <title><?php echo $msg ?></title>
-                        <style type="text/css">
-                        body
-                        {
-                            background: #e9e9e9;
-                            background: <?php echo $bg_color ?>;
-                            margin: 0px;
-                            padding: 0px;
-                        }
 
-                        div 
-                        {
-                            box-shadow: 0px 3px 6px 3px rgba(0,0,0,0.2);
-                            border:1px solid gray;
-                            border-radius:5px;
-                            display: inline-block;
-                            padding:30px;
-                            font-size: 16px;
-                            font: 20px Georgia, "Times New Roman", Times, serif;
-                            width: 460px;
-                            margin: 60px auto;
-                            display: block;
-                            background: white;
-                        }
-                        </style>
+                            <head>
+                                <meta charset="utf-8"/>
+                                <title>Ohlala! It seems that something went wrong</title>
+                                <style type="text/css">
+                                    @font-face {
+                                      font-family: 'segoe';
+                                      src: url('app/resources/fonts/segoeuil.ttf') format('truetype');
+                                    }
 
-                    </head>
-                    <body>
-                        <div><?php echo $msg ?></div>
-                    </body>
+                                    body
+                                    {
+                                        background: #5b1c79;
+                                        margin: 0px;
+                                        padding: 0px;
+                                    }
+
+                                    div 
+                                    {
+                                        box-shadow: 0px 3px 6px 3px rgba(0,0,0,0.2);
+                                        border:1px solid gray;
+                                        border-radius:5px;
+                                        display: inline-block;
+                                        padding:30px;
+                                        font-size: 16px;
+                                        /*font: 20px Georgia, "segoe", Times, serif;*/
+                                        font: 20px "segoe";
+                                        width: 460px;
+                                        margin: 60px auto;
+                                        display: block;
+                                        background: white;
+                                        text-align: center;
+                                    }
+                                </style>
+                            </head>
+                            <body>
+
+                                <div>
+                                    Ohlala! It seems that something went wrong
+                                </div>
+                                
+                            </body>
 
                     <?php
                 }
